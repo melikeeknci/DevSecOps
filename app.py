7c919139e693785043923935b95c08cb981b84d7
Melike Ekinci <82154036+melikeeknci@users.noreply.github.com>
2024-01-31 04:27:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def get_random_fact():
         return jsonify({'error': 'Bilgi alınamadı'}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
